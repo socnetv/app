dac455723b72464eeb4604cf21aa58a1570dd635
Dimitris Kalamaras <dimitris.kalamaras@gmail.com>
2009-01-14 14:55:53 +0000

    update-site script can update manual also...
    git-svn-id: https://svn.code.sf.net/p/socnetv/code@190 fc2d5e9b-b3ce-4657-a66d-b27cdf764933

--- tools/update-site.sh
+++ tools/update-site.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
+echo  --------------------
+echo  . copying webfiles .
+echo  --------------------
 cd ~/Documents/socnetv/site
 rsync -R  -e ssh  *  oxy86,socnetv@web.sourceforge.net:htdocs/
+echo  --------------------
+echo  . copying manual   .
+echo  -------------------- 
+cd ~/Documents/socnetv/trunk/doc
+rsync -R -e ssh * oxy86,socnetv@web.sourceforge.net:htdocs/docs/
