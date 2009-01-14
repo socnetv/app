b160173e80eb9c2aafcdb2745e6549919a720411
Dimitris Kalamaras <dimitris.kalamaras@gmail.com>
2009-01-14 19:52:12 +0000

    NEW LOGO for SocNetV 0.5.! Various site and logo updates
    git-svn-id: https://svn.code.sf.net/p/socnetv/code@192 fc2d5e9b-b3ce-4657-a66d-b27cdf764933

--- tools/update-site.sh
+++ tools/update-site.sh
@@ -3,9 +3,9 @@ echo  --------------------
 echo  . copying webfiles .
 echo  --------------------
 cd ~/Documents/socnetv/site
-rsync -R  -e ssh  *  oxy86,socnetv@web.sourceforge.net:htdocs/
+rsync -Rv -del -e ssh  *  oxy86,socnetv@web.sourceforge.net:htdocs/
 echo  --------------------
 echo  . copying manual   .
 echo  -------------------- 
 cd ~/Documents/socnetv/trunk/doc
-rsync -R -e ssh * oxy86,socnetv@web.sourceforge.net:htdocs/docs/
+rsync -Rv -del -e ssh * oxy86,socnetv@web.sourceforge.net:htdocs/docs/
