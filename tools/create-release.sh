66fa686c7dabca4371ad7cdb1081f46d88bc92aa
Dimitris Kalamaras <dimitris.kalamaras@gmail.com>
2009-02-18 00:21:06 +0000

    added create-release script
    git-svn-id: https://svn.code.sf.net/p/socnetv/code@253 fc2d5e9b-b3ce-4657-a66d-b27cdf764933

---
+++ tools/create-release.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+
+cd ~/Documents/socnetv/trunk
+if [ -d "../release" ]; then
+	echo .
+	echo ---------------------------------
+	echo      REMOVING OLD RELEASE   
+	echo ---------------------------------
+	rm -rf ../release;
+fi
+
+#CHANGE THIS TO NEW VERSION NUMBERS
+VER=0.51;   
+echo $VER
+
+echo .
+echo ---------------------------------
+echo !  CLEANING UP COMPILED FILES   !
+echo ---------------------------------
+
+make clean
+
+echo .
+echo ---------------------------------
+echo 	     RUNNING AUTOCONF
+echo --------------------------------
+autoconf
+
+echo .
+echo ---------------------------------
+echo !  COPY FILES TO WORKING DIRS   ! 
+echo ---------------------------------
+
+find . -not -name "qdevelop-*" -not -name "pajek*" -not -path "*./autom4te.cache*" -not -path "*.svn*" -not -path "*./test-nets*" -not -path "./debian*"  -print0  | cpio -pmd0 ../release/socnetv-$VER
+
+
+
+echo .
+echo ---------------------------------
+echo !  GOTO TO WORKING DIRECTORY    !
+echo ---------------------------------
+
+cd ../release/
+echo "Make tarballs? (Y/N)"
+read ans
+if [ $ans = "N" ]; then
+        exit;
+
+elif [ $ans = "n" ]; then    
+        exit;
+fi
+
+tar zcfv SocNetV-$VER.tar.gz socnetv-$VER/
+tar jcfv SocNetV-$VER.tar.bz2 socnetv-$VER/
+
+
+
+echo "Upload tarballs to Sourceforge also? (Y/N)"
+read ans
+if [ $ans = "N" ]; then
+        exit;
+
+elif [ $ans = "n" ]; then    
+        exit;
+fi
+
+echo ---------------------------------
+echo ! UPLOAD TO SOURCEFORGE         !
+echo ---------------------------------
+echo .
+
+rsync -avP -e ssh SocNetV-$VER.tar.gz  oxy86@frs.sourceforge.net:uploads/
+rsync -avP -e ssh SocNetV-$VER.tar.bz2  oxy86@frs.sourceforge.net:uploads/
+cd socnetv-$VER/
+
+
