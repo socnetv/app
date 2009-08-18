7c9f38290b611681ecff8a210b1da09a50a4e232
Dimitris Kalamaras <dimitris.kalamaras@gmail.com>
2009-08-18 09:08:25 +0000

    updated fedora package creation script
    git-svn-id: https://svn.code.sf.net/p/socnetv/code@376 fc2d5e9b-b3ce-4657-a66d-b27cdf764933

--- tools/create-fedora-package.sh
+++ tools/create-fedora-package.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #CHANGE THIS TO NEW VERSION NUMBERS
-VER=0.52;
+VER=0.80;
 echo $VER
 
 echo .
@@ -12,20 +12,22 @@ echo   License: GNU GPL v3
 echo ---------------------------------
 
 echo Testing if rpmlint and rpmdevtools are installed...
-ans=`rpm -qa | grep rpmlint `
-if [ $ans =  "rpmlint" ]; then
+
+if [ -f /usr/bin/rpmlint ] &&  [ -f /usr/bin/rpmdev-setuptree  ] && [ -f /usr/bin/autoconf ]; then
         echo OK. Necessary tools are installed;
 else
         echo No rpmlint or rpmdevtools. Enter sudo password to install them: 
-        sudo yum install rpmlint rpmdevtools  autoconf
+        su -c 'yum install rpmlint rpmdevtools  autoconf'
 fi
 
 
 
 echo
 echo Checking if home RPM development tree exists...
-if [ -d ~/rpmbuild/SOURCES ];    then
-	echo Home RPM development tree exists. Continuing.
+if [ -d ~/rpmbuild/ ];    then
+	echo Home RPM development tree exists. Recreating it....
+	rm -rf ~/rpmbuild
+	rpmdev-setuptree
 else
         echo Seems there is no RPM development tree. Creating it...
         rpmdev-setuptree
@@ -35,7 +37,7 @@ fi
 
 
 
-cd ~/Documents/socnetv/trunk
+cd ~/socnetv/trunk
 
 
 echo Checking for old working dir...
@@ -47,53 +49,31 @@ else
 fi
 
 
-oldbz2=`find ~/rpmbuild/SOURCES/  -type f -name *.bz2`;
-for file in $oldbz2; do
-        if [ -f "$file" ];  then
-                echo old bz2 $file;
-                echo Seems there are old bz2 in ~... Enter password to remove them
-                sudo rm $file
-        else
-                echo No older bz2s.Continuing....
-                exit;
-        fi
-done
-
-
-
-
-oldrpm=`find ~/rpmbuild/RPMS/  -type f -name *.rpm`;
-for file in $oldrpm; do
-        if [ -f "$file" ];  then
-                echo old RPM $file;
-                echo Seems there are old rpms in ~... Enter password to remove them
-                sudo rm $file
-        else
-                echo No older RPM packages.Continuing....
-                exit;
-        fi
-done
-
-
-
-echo Checking for old SPECs...
-if [ -f ~/rpmbuild/SPECS/*.spec ];    then
-        echo Removing old specs
-	rm ~/rpmbuild/SPECS/socnetv.spec ;
-else
-        echo No older specs. Continuing....
-fi
-
-
 echo .
 echo ---------------------------------
 echo    CLEANING UP COMPILED FILES   
 echo ---------------------------------
 
+autoconf
+
+echo ""
+
 ./configure > /dev/null 2>&1
 
+echo  ""
+
 make clean
-rm socnetv 
+
+echo ""
+
+echo "testing if a binary exists..."
+
+if [ -f socnetv ]; then
+	 rm socnetv 
+else
+	echo "No socnetv binary"
+fi
+
 
 echo removing old backup files
 oldfiles=`find . -type f -name *~`;
@@ -104,7 +84,7 @@ done;
 
 chmod 644 nets/*
 find . -type f -name '*~' -delete
-rm -f config.log config.status Makefile socnetv.spec socnetv.mak
+rm -f config.log config.status Makefile socnetv.mak
 
 echo .
 echo ---------------------------------
@@ -152,10 +132,11 @@ echo    START PACKAGE CREATION
 echo ---------------------------------
 
 echo Copying spec to ~/rpmbuild/SPECS/
+
 cp socnetv.spec ~/rpmbuild/SPECS/
    
 
-if [ -f ~/rpmbuild/SPECS/*.spec ];    then
+if [ -f ~/rpmbuild/SPECS/socnetv.spec ];    then
         echo spec file copied ok;
 else
         echo Sorry. No spec file copied....
@@ -166,7 +147,7 @@ fi
 echo Copying bz2 to ~/rpmbuild/SOURCES/
 cp ../*.bz2  ~/rpmbuild/SOURCES/
 
-if [ -f ~/rpmbuild/SOURCES/*.bz2 ];    then
+if [ -f ~/rpmbuild/SOURCES/SocNetV-$VER.tar.bz2 ];    then
         echo bz2 file copied ok;
 else
         echo Sorry. No bz2 file copied....
@@ -187,12 +168,13 @@ echo Enter password to start package creation
 
 rpmbuild -ba socnetv.spec
 
+ls -l ~/rpmbuild/RPMS/i386/*.rpm > /dev/null 2>&1
 
-if [ -f ~/rpmbuild/RPMS/i386/*.rpm ];  	then
-	echo file ok; 
+if [ "$?" = "0" ]; then 
+	echo "file ok";
 else 
-	echo Sorry. No RPM package....
-	exit;
+	echo "Sorry. No RPM package..."
+	exit
 fi
 
 echo .
