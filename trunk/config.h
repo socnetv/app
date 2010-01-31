40c91996a4975af89fa3a3b7f953334ac3a51cfa
Dimitris Kalamaras <dimitris.kalamaras@gmail.com>
2010-01-31 17:00:23 +0000

    minor changes to a test graphm
    git-svn-id: https://svn.code.sf.net/p/socnetv/code@538 fc2d5e9b-b3ce-4657-a66d-b27cdf764933

--- trunk/config.h
+++ trunk/config.h
@@ -2,7 +2,7 @@
 /* config.h.in.  Generated from configure.ac by autoheader.  */
 
 /* Define if debugging is enabled. */
-#define CONFIG_DEBUG 1
+/* #undef CONFIG_DEBUG */
 
 /* Define if IEEE 32bit float optimizations are enabled. */
 #define CONFIG_FLOAT32 1
