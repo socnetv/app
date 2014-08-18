d22b17389903adf13ec3669c3c0ed1d5ce8e4132
Dimitris Kalamaras <dimitris.kalamaras@gmail.com>
2014-08-18 15:24:01 +0300

    reverted to setflat(false) - it was ugly as hell :)

--- trunk/src/mainwindow.cpp
+++ trunk/src/mainwindow.cpp
@@ -1913,7 +1913,6 @@ void MainWindow::initToolBox(){
     QGroupBox *editGroupBox= new QGroupBox(tr("Edit"));
     editGroupBox->setLayout(buttonsGrid);
     editGroupBox->setMaximumSize(300,100);
-    editGroupBox->setFlat(true);
 
     //create widgets for the "Analysis" box
     QLabel *toolBoxAnalysisGeodesicsSelectLabel = new QLabel;
@@ -1980,7 +1979,6 @@ void MainWindow::initToolBox(){
     QGroupBox *analysisBox= new QGroupBox(tr("Analyze"));
     analysisBox->setMaximumWidth(300);
     analysisBox->setLayout (analysisGrid );
-    analysisBox->setFlat(true);
 
 
     //create widgets for the "Visualization By Index" box
@@ -2027,7 +2025,6 @@ void MainWindow::initToolBox(){
     QGroupBox *layoutByIndexBox= new QGroupBox(tr("By Prominence Index"));
     layoutByIndexBox->setMaximumWidth(300);
     layoutByIndexBox->setLayout (layoutByIndexGrid );
-    layoutByIndexBox->setFlat(true);
 
 
     // create widgets for the "Dynamic" groupBox
@@ -2069,7 +2066,6 @@ void MainWindow::initToolBox(){
     QGroupBox *layoutDynamicBox= new QGroupBox(tr("By Dynamic Model"));
     layoutDynamicBox->setMaximumWidth(300);
     layoutDynamicBox->setLayout (layoutDynamicGrid );
-    layoutDynamicBox->setFlat(true);
 
 
     //create widgets for additional visualization options box
@@ -2109,7 +2105,6 @@ void MainWindow::initToolBox(){
     QGroupBox *layoutOptionsBox= new QGroupBox(tr("Options"));
     layoutOptionsBox->setMaximumWidth(300);
     layoutOptionsBox->setLayout (layoutOptionsGrid );
-    layoutOptionsBox->setFlat(true);
 
 
     //Parent box with vertical layout for all layout/visualization boxes
@@ -2120,7 +2115,6 @@ void MainWindow::initToolBox(){
     QGroupBox *visualizationBox= new QGroupBox(tr("Visualize"));
     visualizationBox->setMaximumWidth(300);
     visualizationBox->setLayout (visualizationBoxLayout );
-    visualizationBox->setFlat(true);
 
     //Parent box with vertical layout for all boxes of Controls
     QVBoxLayout *controlTabVerticalLayout = new QVBoxLayout;
