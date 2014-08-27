2b92781b31220b914074b4c504ad501876c0e569
Dimitris Kalamaras <dimitris.kalamaras@gmail.com>
2014-08-27 12:27:15 +0300

    - SocNetV can now load and display networks of 1000 actors and 10000 edges without problem using under 400MB RAM - fixed bug crashing app on Matrix::resize() - moved Matrix function definition from .h to the.cpp file - code appearance optimization - added new methods (i.e. Matrix::clear()) , removed obsolete ones (i.e. Matrix::setSize() ) - added Q_CHECK_PTR on every new pointer creation

--- trunk/src/matrix.h
+++ trunk/src/matrix.h
@@ -34,31 +34,34 @@
 #include <QDebug>
 #include <QTextStream>
 
+using namespace std; //or else compiler groans for nothrow
 
 class Row {
 public:
-    Row (int Actors=3000) {
-		cell=new  float [m_Actors=Actors];
+    Row (int Actors=0) {
+        cell=new (nothrow) float [m_Actors=Actors];
 		Q_CHECK_PTR( cell );
         for (register int i=0;i<m_Actors; i++) {
             cell[i]=0;
         }
 		m_outEdges=0;
 	}
-	~Row() {delete [] cell;}
+
+    ~Row() { m_Actors=0, m_outEdges=0 ; delete [] cell;}
 	
-	Row& operator =(Row & a) {
-			if (this != &a){
-				if (a.m_Actors!=m_Actors) {
-					delete [] cell;
-					cell=new  float[m_Actors=a.m_Actors];
-					Q_CHECK_PTR( cell);
-				}
-				for (int i=0;i<m_Actors; i++)
-				    cell[i]=a.cell[i];
-			}
-			return *this;
-	}
+    Row& operator =(Row & a) {
+        if (this != &a){
+            if (a.m_Actors!=m_Actors) {
+                delete [] cell;
+                cell=new (nothrow) float[m_Actors=a.m_Actors];
+                Q_CHECK_PTR( cell);
+            }
+            for (int i=0;i<m_Actors; i++) {
+                    cell[i]=a.cell[i];
+            }
+        }
+        return *this;
+    }
 	
 	float column ( int c ) const {
 		return cell[c];
@@ -88,9 +91,8 @@ public:
 
 	void resize(int Actors) {
 		delete [] cell;
-		cell=new  float[m_Actors=Actors];
+        cell=new (nothrow) float[m_Actors=Actors];
 		Q_CHECK_PTR( cell);
-		
 		for (int i=0;i<m_Actors; i++) { 
 			cell[i]=0;
 		}
@@ -112,87 +114,75 @@ private:
 
 
 class Matrix {
-	public:
-        /**default constructor - creates a Matrix of size 3000
-         *  Use resize(int) to resize it*/
-        Matrix (int Actors=0)   {
-            row=new Row[m_Actors=Actors];
-            Q_CHECK_PTR( row );
-        }
+public:
+    Matrix (int Actors=0) ; /**default constructor - default size 0 */
 
-		Matrix(const Matrix &b) ;	/* Copy constructor allows Matrix a=b  declaration */
+    Matrix(const Matrix &b) ;	/* Copy constructor allows Matrix a=b  */
 
-		~Matrix() { delete [] row; }	/* Destructor */
+    ~Matrix();
 
-        void clear() { delete [] row;}
+    void clear();
 
-		void setSize (int Actors);	/* Used to set m_Actors */
-			
-		float item( int r, int c ) ;
-		
-		void setItem( int r, int c, float elem );
-		
-		void clearItem( int r, int c ) ;
-		
-		int cols() {return m_Actors;}
-		
-		int rows() {return m_Actors;}
+    void resize (int Actors) ;
 
-        int size() { return m_Actors;}
+    float item( int r, int c ) ;
 
-        void findMinMaxValues(float&,float&);
-	
-		void deleteRowColumn(int i);	/* deletes row i and column i */
-	
-		void resize (int Actors) ;	/* This is called before every operation on new matrixes. */
-	
+    void setItem( int r, int c, float elem );
 
-		int edgesFrom(int Actor);
+    void clearItem( int r, int c ) ;
 
-		int edgesTo(int Actor);
-		
-		int totalEdges();
+    int cols() {return m_Actors;}
 
-		bool printMatrixConsole();
+    int rows() {return m_Actors;}
 
-		void identityMatrix (int);
+    int size() { return m_Actors;}
 
-        void zeroMatrix (int);
+    void findMinMaxValues(float&,float&);
 
-		void fillMatrix (float value );	   /* Fulls a matrix with a value */
-	
-		Matrix& operator =(Matrix & a);	    /* Equals two matrices. */
+    void deleteRowColumn(int i);	/* deletes row i and column i */
 
-        Matrix& operator +(Matrix & b);	    /* adds two matrices and returns their sum. */
+    int edgesFrom(int Actor);
 
-        Matrix operator *(Matrix & b);	    /* multiplies two matrices and returns their sum. */
+    int edgesTo(int Actor);
 
-		float  operator ()  (const int r, const int c) ;
-		
-		friend QTextStream& operator <<  (QTextStream& os, Matrix& m);
+    int totalEdges();
 
-		/** Takes two matrices and returns their product as a reference */
-        Matrix & product( Matrix &a, Matrix & b, bool symmetry) ;
-		
-		/** Takes two symm. matrices and outputs an upper triangular matrix */
-        Matrix & productSym( Matrix &a, Matrix & b)  ;
-		
-		Matrix & pow (int power, bool symmetry)  ;
+    bool printMatrixConsole();
 
-		Matrix& subtractFromI () ;
+    void identityMatrix (int);
 
-		Matrix& sum (Matrix &a, Matrix &b) ;
+    void zeroMatrix (int);
 
-		Matrix& inverseByGaussJordanElimination(Matrix &a);
+    void fillMatrix (float value );
 
-		void swapRows(int rowA,int rowB);		/* elementary matrix algebra */
-		void multiplyRow(int row, float value);		/* Multiply every elememt of row A by value */
+    Matrix& operator =(Matrix & a);
 
-	private:
-		Row *row;
-		int m_Actors;
-		int *vec;
+    Matrix& operator +(Matrix & b);
 
+    Matrix operator *(Matrix & b);	    // undefined
+
+    float  operator ()  (const int r, const int c) ;
+
+    friend QTextStream& operator <<  (QTextStream& os, Matrix& m);
+
+    Matrix & product( Matrix &a, Matrix & b, bool symmetry) ;
+
+    Matrix & productSym( Matrix &a, Matrix & b)  ;
+
+    Matrix & pow (int power, bool symmetry)  ;
+
+    Matrix& subtractFromI () ;
+
+    Matrix& sum (Matrix &a, Matrix &b) ;
+
+    Matrix& inverseByGaussJordanElimination(Matrix &a);
+
+    void swapRows(int rowA,int rowB);		/* elementary matrix algebra */
+    void multiplyRow(int row, float value);		/* Multiply every elememt of row A by value */
+
+private:
+    Row *row;
+    int m_Actors;
 };
 
 
