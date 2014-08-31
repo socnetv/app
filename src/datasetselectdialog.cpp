2a2f8b072a6d09373785d4e0f7ccced67e787969
Dimitris Kalamaras <dimitris.kalamaras@gmail.com>
2014-08-31 23:51:09 +0300

    * added datasets: Freeman's EIES networks (multirelational, 32 actors) Freeman: EIES network, at time-1, 48 actors Freeman: EIES network, at time-2, 48 actors Freeman: EIES network, number of messages, 48 actors

--- src/datasetselectdialog.cpp
+++ src/datasetselectdialog.cpp
@@ -39,21 +39,22 @@ DataSetSelectDialog::DataSetSelectDialog (QWidget *parent) : QDialog (parent)
     (ui.buttonBox) -> button (QDialogButtonBox::Ok) -> setDefault(true);
 
     datasets_list
-            << "Krackhardt: High-tech managers (advice)"
-            << "Krackhardt: High-tech managers (friendship)"
-            << "Krackhardt: High-tech managers (Reports To)"
-            << "Padgett: Florentine Families (marital)"
-            << "Padgett: Florentine Families (business)"
-            << "Zachary: Karate Club (simple ties)"
-            << "Zachary: Karate Club (weighted ties)"
-            << "Bernard: Killworth Fraternity"
+            << "Krackhardt: High-tech managers (advice), 24 actors"
+            << "Krackhardt: High-tech managers (friendship), 24 actors"
+            << "Krackhardt: High-tech managers (Reports To), 24 actors"
+            << "Padgett: Florentine Families (marital relationship), 16 actors"
+            << "Padgett: Florentine Families (business relationship), 16 actors"
+            << "Zachary: Karate Club (simple ties), 34 actors"
+            << "Zachary: Karate Club (weighted ties), 34 actors"
+            << "Bernard: Killworth Fraternity (multirelational), 58 actors"
             << "Galaskiewicz: CEOs and clubs (affiliation data)"
-            << "Freeman: EIES network (acquaintanceship at time-1)"
-            << "Freeman: EIES network (acquaintanceship at time-2)"
-            << "Freeman: EIES network (messages)"
-            << "Mexican Power Network in the 1940s"
-            << "Knocke: Bureacracies Information Exchange Network"
-            << "Wasserman & Faust: Countries Trade (basic manufactured goods)";
+            << "Freeman's EIES networks (multirelational, 32 actors)"
+            << "Freeman: EIES network, at time-1, 48 actors"
+            << "Freeman: EIES network, at time-2, 48 actors"
+            << "Freeman: EIES network, number of messages, 48 actors"
+            << "Mexican Power Network in the 1940s (list format)"
+            << "Knocke: Bureacracies Information Exchange Network, 10 actors"
+            << "Wasserman & Faust: Countries Trade (basic manufactured goods), 24 actors";
 
     datasets_filenames
             << "Krackhardt_High-tech_managers_Advice_relation.sm"
@@ -65,9 +66,10 @@ DataSetSelectDialog::DataSetSelectDialog (QWidget *parent) : QDialog (parent)
             << "Zachary_Karate_Club_Weighted_Ties.sm"
             << "Bernard_Killworth_Fraternity.dl"
             << "Galaskiewicz_CEOs_and_clubs_affiliation_network_data.2sm"
-            << "Freeman_EIES_network_Acquaintanceship_at_time-1"
-            << "Freeman_EIES_network_Acquaintanceship_at_time-2"
-            << "Freeman_EIES_network_Messages"
+            << "Freeman_EIES_networks_32actors.dl"
+            << "Freeman_EIES_network_48actors_Acquaintanceship_at_time-1.dl"
+            << "Freeman_EIES_network_48actors_Acquaintanceship_at_time-2.dl"
+            << "Freeman_EIES_network_48actors_Messages.dl"
             << "Mexican_Power_Network_1940s.lst"
             << "Knocke_Bureacracies_Information_Exchange_Network.pajek"
             << "Wasserman_Faust_Countries_Trade_Data_Basic_Manufactured_Goods.pajek";
