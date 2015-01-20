d3b2e7e51bd7520b934227dd58706deaf2f45aa9
Dimitris Kalamaras <dimitris.kalamaras@gmail.com>
2015-01-20 19:43:40 +0200

    started recoding web crawler

--- src/webcrawler.cpp
+++ src/webcrawler.cpp
@@ -41,13 +41,14 @@ QNetworkReply *reply;
 
 QQueue<QString> frontier;
 QMap <int, int> sourceMap;
-QMap <QString, bool> checkedMap;
-QMap <QString, int> discoveredMap;
+QMap <QString, bool> visitedUrls;
+QMap <QString, int> knownUrls;
 QByteArray ba;
 QWaitCondition newDataRead;
 QMutex mutex;
-QString baseUrl="", seed="", domain="", seed_domain = "", previous_domain="", path="", urlPrefix="";
-int maxNodes, discoveredNodes=0, currentNode, maxRecursion;
+QString currentUrl="", seed="", domain="", seed_domain = "", previous_domain="";
+QString path="", urlPrefix="";
+int maxPages, discoveredNodes=0, currentNode, maxRecursion;
 bool goOut=false, hasUrlPrefix=false;
 
 void WebCrawler::load (QString url, int maxN, int maxRec, bool gOut){ 
@@ -55,17 +56,17 @@ void WebCrawler::load (QString url, int maxN, int maxRec, bool gOut){
     if (seed.contains(" "))		//urls can't have spaces...
         return;
 
-    seed=url;
-    maxNodes=maxN;		//maxnodes we'll check
+    seed=url;       //the initial url/domain we will crawl
+    maxPages=maxN;		//maxPages we'll check
     maxRecursion = maxRec;
     goOut = gOut;
 
     //clear global variables
     frontier.clear();
     sourceMap.clear();
-    checkedMap.clear();
-    discoveredMap.clear();
-    baseUrl="", domain="", previous_domain="", path="", urlPrefix="";
+    visitedUrls.clear();
+    knownUrls.clear();
+    currentUrl="", domain="", previous_domain="", path="", urlPrefix="";
 
     frontier.enqueue(seed);		//queue the seed to frontier
     currentNode=1;			//start at node 1
@@ -104,107 +105,142 @@ void WebCrawler::slotCreateEdge (int source, int target){
 
 
 
-/*	 This thread parses urls from frontier and downloads their data.
-* 	When http signals finished() then reader thread is loaded
+/*
+ * Parses urls from frontier and downloads their data.
+*  When http signals finished() then reader thread is loaded
 *  to parse the data
 */
 void WebCrawler::run(){
-    int pos, second_pos;
-    Q_UNUSED(second_pos);
+    int pos;
+
     do { 	//repeat forever....
 
-        if (currentNode>1 ) {
+        if ( currentNode > 1 ) {
             maxRecursion --;
         }
 
-        if (frontier.size() ==0 ) { 	// .... or until we crawl all urls in frontier.
-            qDebug () <<"		WebCrawler #### Frontier is empty: " <<frontier.size() << " - we will stop now "  ;
+        if (frontier.size() ==0 ) {     //  until we crawl all urls in frontier.
+            qDebug () <<"	WebCrawler #### Frontier is empty: "
+                     <<frontier.size() << " - we will stop now "  ;
             break;
         }
 
-        if ( maxRecursion  == 0 ) { 	// .... or until we reach maxRecursion
-            qDebug () <<"		WebCrawler #### Reached maxRecursion: " <<maxRecursion << " - we will stop now "  ;
+        if ( maxRecursion  == 0 ) {     //  or until we reach maxRecursion
+            qDebug () <<"	WebCrawler #### Reached maxRecursion: "
+                     <<maxRecursion << " - we will stop now "  ;
             break;
         }
 
-        if (maxNodes>0) {  				// .... or until we have reached maxNodes
-            if (currentNode == maxNodes ) {
-                qDebug () <<"		WebCrawler: #### Seems we have reached maxnodes!" << " - we will stop now" ;
+        if (maxPages>0) {               // or until we have reached maxPages
+            if (currentNode == maxPages ) {
+                qDebug () <<"	WebCrawler: #### Seems we have reached maxPages!"
+                         << " - we will stop now" ;
                 break;
             }
         }
 
+        //take the first url from frontier - name it currentUrl
+        currentUrl = frontier.head();
 
-        baseUrl = frontier.head();	//take the first url from the frontier - name it baseUrl
+        if ( ! visitedUrls[currentUrl] ) {
+            // currentUrl has not been crawled yet. Check what it is.
+            qDebug() << "	WebCrawler: currentUrl "  <<  currentUrl.toLatin1()
+                     << " not visited. Adding it to visitedUrls - Checking it.";
 
-        if ( ! checkedMap[baseUrl] ){
+            visitedUrls[currentUrl]=true;
 
-            checkedMap[baseUrl]=true;
+            if ( currentUrl.contains ("//" ) ) {
+                qDebug() << "	WebCrawler: currentUrl probably external: "
+                         <<currentUrl;
 
-            if (baseUrl.contains ("http://" ) || baseUrl.contains ("https://" )) {
-                if  ( currentNode != 1 && !goOut  ) {	// if the user don't want to crawl external links...
-                    if (  !baseUrl.contains( seed_domain, Qt::CaseInsensitive)   ) { // and baseUrl is external, continue
-                        qDebug() << "		WebCrawler: external baseUrl detected: "<<baseUrl
-                                 << " But we will not crawl it. Continuing...";
-                        continue;
+                if ( !currentUrl.contains( seed_domain, Qt::CaseInsensitive) ) {
+
+                    qDebug() << "	WebCrawler: unknown external currentUrl : "
+                             <<currentUrl;
+
+                    if  ( currentNode != 1 && !goOut  ) {
+                        // if the user don't want to crawl external links...
+                         qDebug() << " We will not crawl it. Continuing...";
+                         continue;
+
                     }
-                }
-                else if ( currentNode == 1){
-                    qDebug() << "		WebCrawler: creating node 1 with label " << baseUrl.toLatin1();
-                    discoveredMap[baseUrl]=currentNode;
-                    emit createNode(baseUrl, 1);
-                }
-                qDebug() << "		WebCrawler: external baseUrl detected: "<<baseUrl;
-                domain=baseUrl.remove ("http://");
-                if ( (pos=domain.indexOf ("/")) !=-1 ) { //find new domain
-                    domain = domain.left(pos);
-                    qDebug() << "		WebCrawler: Host domain is the url: " << domain.toLatin1();
-                }
-                else {
-                    qDebug() << "		WebCrawler: Host domain is the url: " << domain.toLatin1() ;
-                    seed_domain = domain;
-                }
+                    else if ( currentNode == 1 ){
+                        qDebug() << "	WebCrawler: creating node 1 with label "
+                                 << currentUrl.toLatin1();
 
+                        knownUrls[currentUrl]=currentNode;
+
+                        emit createNode(currentUrl, 1);
+                    }
+                    if ( currentUrl.contains ("http://" ) )
+                        domain=currentUrl.remove ("http://");
+
+                    if ( currentUrl.contains ("https://" ) )
+                        domain=currentUrl.remove ("https://");
+
+                    if ( currentUrl.contains ("//" ) )
+                        domain=currentUrl.remove ("//");
+
+                    if ( (pos=domain.indexOf ("/")) !=-1 ) { //find new domain
+                        domain = domain.left(pos);
+                        qDebug() << "		WebCrawler: Host domain is the url: "
+                                 << domain.toLatin1();
+                    }
+                    else {
+                        qDebug() << "		WebCrawler: Host domain is the url: "
+                                 << domain.toLatin1() ;
+                        seed_domain = domain;
+                    }
+
+
+                }
             }
-            else { //no http:// in baseUrl - this is an internal node
+            else { //no http:// in currentUrl - this is an internal node
                 if (currentNode==1) {		//only if this is the seed node
-                    if ( (pos=baseUrl.indexOf ("/")) !=-1 ) {
-                        domain = baseUrl.left(pos);
-                        qDebug() << "		WebCrawler: Initial Host domain: "<<  domain.toLatin1();
-                        qDebug() << "		WebCrawler: Initial Webpage to get: "<<  baseUrl.toLatin1() ;
+                    if ( (pos=currentUrl.indexOf ("/")) !=-1 ) {
+                        domain = currentUrl.left(pos);
+                        qDebug() << "		WebCrawler: Initial Host domain: "
+                                 <<  domain.toLatin1();
+                        qDebug() << "		WebCrawler: Initial Webpage to get: "
+                                 <<  currentUrl.toLatin1() ;
                         seed_domain = domain;
                     }
                     else {
-                        qDebug() << "		WebCrawler: Initial url: " << baseUrl.toLatin1() << " I' ll just get /...";
-                        domain = baseUrl;
+                        qDebug() << "		WebCrawler: Initial url: "
+                                 << currentUrl.toLatin1() << " I' ll just get /...";
+                        domain = currentUrl;
                     }
-                    emit createNode(baseUrl, 1);
+                    emit createNode(currentUrl, 1);
                 }
                 else {
-                    qDebug() << "		WebCrawler: internal url detected " << baseUrl.toLatin1() << " I will use previous domain "<< domain.toLatin1();
-                    if (baseUrl.startsWith('.', Qt::CaseInsensitive) )
-                        baseUrl=baseUrl.remove(0, 1);
-                    else if (!baseUrl.startsWith('/',  Qt::CaseInsensitive) ) {
-                        baseUrl = "/"+baseUrl;
-                        qDebug() << "			adding / to baseUrl " << baseUrl;
+                    qDebug() << "		WebCrawler: internal url detected "
+                             << currentUrl.toLatin1() << " I will use previous domain "
+                             << domain.toLatin1();
+                    if (currentUrl.startsWith('.', Qt::CaseInsensitive) )
+                        currentUrl=currentUrl.remove(0, 1);
+                    else if (!currentUrl.startsWith('/',  Qt::CaseInsensitive) ) {
+                        currentUrl = "/"+currentUrl;
+                        qDebug() << "			adding / to currentUrl " << currentUrl;
                     }
                 }
             }
-            // download baseUrl
+            // download currentUrl
             http = new QNetworkAccessManager(this);
             request = new QNetworkRequest;
-            request->setUrl(QUrl(baseUrl));
+            request->setUrl(QUrl(currentUrl));
 
 
             //TODO connect finished() signal to load() of 2ond Reader class
-            connect (http,SIGNAL( finished(QNetworkReply*) ), &reader, SLOT( load(QNetworkReply*) ) );
+            connect (http,SIGNAL( finished(QNetworkReply*) ),
+                     &reader, SLOT( load(QNetworkReply*) ) );
 
-            QNetworkReply *reply = http->get(*request) ;
-            Q_UNUSED(reply);
+            http->get(*request) ;
 
         }
-        else { // baseUrl is on checkedMap already - don't do nothing!
-            qDebug() << "		WebCrawler: baseUrl "  <<  baseUrl.toLatin1() << " already scanned. Skipping.";
+        else {
+            // currentUrl has been crawled already - don't do nothing!
+            qDebug() << "	WebCrawler: currentUrl "  <<  currentUrl.toLatin1()
+                     << " already visited. Skipping.";
             frontier.dequeue();			//Dequeue head
             continue;
         }
@@ -226,8 +262,6 @@ void WebCrawler::run(){
         //Unlock it
         mutex.unlock();
         qDebug () <<"		WebCrawler: OK. Waking up to continue: frontier size = " << frontier.size();
-
-
 
 
         qDebug () <<"		WebCrawler: Increasing currentNode, dequeuing frontier and setting previous domain to domain";
@@ -278,15 +312,15 @@ void Reader::run(){
     qDebug()  << "			READER: read something!";
     QString newUrl;
     bool createNodeFlag = false, createEdgeFlag=false ;
-    Q_UNUSED(createNodeFlag);
-    Q_UNUSED(createEdgeFlag);
+//    Q_UNUSED(createNodeFlag);
+//    Q_UNUSED(createEdgeFlag);
     int start=-1, end=-1, equal=-1 ;// index=-1;
     QString page(ba);
 
 
     if (!page.contains ("a href"))  { //if a href doesnt exist, return
         //FIXME: Frameset pages are not parsed! See docs/manual.html for example.
-        qDebug() << "			READER: ### Empty or not useful data from " << baseUrl.toLatin1() << " RETURN";
+        qDebug() << "			READER: ### Empty or not useful data from " << currentUrl.toLatin1() << " RETURN";
         newDataRead.wakeAll();
         return;
     }
@@ -323,8 +357,8 @@ void Reader::run(){
 
         qDebug()<< "			READER: page still contains links - Parsing " << newUrl.toLatin1();
         // if this is not the 1st node, and it has been already checked ...
-        QMap<QString, int>::const_iterator index = discoveredMap.find(newUrl);
-        if (   index!= discoveredMap.end() ) {
+        QMap<QString, int>::const_iterator index = knownUrls.find(newUrl);
+        if (   index!= knownUrls.end() ) {
             qDebug()<< "			READER: #---> newUrl "  <<  newUrl.toLatin1()
                     << " already CHECKED - Just creating an edge from " << currentNode << " to " << index.value();
             //this->createEdge (sourceMap [ index.value() ], index.value());	// ... then create an edge from the previous node ...
@@ -350,7 +384,7 @@ void Reader::run(){
                 {
                     qDebug()<< "			READER: # absolute newUrl " << newUrl
                             << " must be a web 2.0 element (rss, favicon, etc) or file. Skipping...";
-                    //	emit createNode(baseUrl, currentNode);
+                    //	emit createNode(currentUrl, currentNode);
                     //  perhaps we paint that node with a different colour or check a variable?
                     //continue;
                 }
@@ -406,7 +440,7 @@ void Reader::run(){
                 {
                     qDebug()<< "			READER: # non-absolute newUrl " << newUrl
                             << " must be a web 2.0 element (rss, favicon, etc) or file. Skipping...";
-                    //	emit createNode(baseUrl, currentNode);
+                    //	emit createNode(currentUrl, currentNode);
                     //  perhaps we paint that node with a different colour or check a variable?
                     continue;
                 }
@@ -431,7 +465,7 @@ void Reader::run(){
 void Reader::createNode(QString newUrl, bool enqueue_to_frontier) {
     discoveredNodes++;
     sourceMap[ discoveredNodes ] = currentNode;
-    discoveredMap[newUrl]=discoveredNodes;
+    knownUrls[newUrl]=discoveredNodes;
     if (enqueue_to_frontier) {
         frontier.enqueue(newUrl);
         qDebug()<< "\n\n		READER: * Creating node " << discoveredNodes
