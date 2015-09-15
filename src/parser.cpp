778a8713c2f7ab0a7966de0f1f46bfc2569c918a
Dimitris Kalamaras <dimitris.kalamaras@gmail.com>
2015-09-15 16:08:58 +0300

    triaging bug in ucinet dl format parser

--- src/parser.cpp
+++ src/parser.cpp
@@ -391,7 +391,8 @@ bool Parser::loadDL(){
             lineElement=str.split(QRegExp("\\s+"), QString::SkipEmptyParts);
 
             if (lineElement.count() < aNodes ) {
-                qDebug() << "This line has fewer than " << aNodes << " edges";
+                qDebug() << "This line has " << lineElement.count()
+                         << " elements, expected " << aNodes << " - appending next line";
                 prevLineStr=str;
                 continue;
             }
