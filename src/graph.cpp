socnetv/app

commit 547b396798ddceb6933d3f794899bf138f3d7c4a

Fixed #26 - Fixed unsetting error effect in Node Properties textedit when the user selects a proper custom node icon. Trying to pinpoint bug when default icons are used,and you load a net with custom icons,but new nodes have blankicons

src/graph.cpp
@@ -720,8 +720,8 @@ void Graph::vertexCreate(const int &num,
     qDebug() << "Graph::vertexCreate() - vertex:" << num
              << "shape:" << nodeShape
              << "icon:" << nodeIconPath
-                << "signalMW:" << signalMW
-                   << "- Calling vertexAdd() and emitting signalDrawNode() to GW";
+             << "signalMW:" << signalMW
+             << "- Calling vertexAdd() and emitting signalDrawNode() to GW";
 
     vertexAdd ( num,
                 value,
@@ -755,10 +755,14 @@ void Graph::vertexCreate(const int &num,
 
     graphSetModified(GraphChange::ChangedVertices, signalMW);
 
-    //draw new user-clicked nodes with the same color with that of the file loaded
+    //to draw new user-clicked nodes with the same style of the file loaded:
+    //save color, size and shape as init values
     initVertexColor=nodeColor;
-    initVertexShape=nodeShape;
     initVertexSize=nodeSize;
+    initVertexShape=nodeShape;
+    if (nodeShape=="custom"){
+        initVertexIconPath=nodeIconPath;
+    }
 } 
 
 
@@ -14913,7 +14917,8 @@ bool Graph::graphSaveToGraphMLFormat (const QString &fileName,
 
     QString saveDirPath= fileInfo.canonicalPath();
 
-    qDebug () << "Graph::graphSaveToGraphMLFormat() - Will save network to directory:" << saveDirPath;
+    qDebug () << "Graph::graphSaveToGraphMLFormat() - Save to directory:"
+              << saveDirPath;
 
     QString iconsSubDir = fileInfo.baseName() + "_" + fileInfo.suffix() +"_images";
     QString iconsDirPath = saveDirPath + "/" + iconsSubDir;
@@ -14922,12 +14927,18 @@ bool Graph::graphSaveToGraphMLFormat (const QString &fileName,
 
     // Check if there are nodes with custom icons in the network
     if ( graphHasVertexCustomIcons()) {
+        qDebug () << "Graph::graphSaveToGraphMLFormat() - Custom node icons exist."
+                  <<  "Creating images subdir" << iconsDirPath;
         // There are custom node icons in this net.
         // We need to save these custom icons to a folder
         // Create a subdir inside the directory where the actual network file
         // is about to be saved. All custom icons will be copied one-by-one there.
         if ( saveDir.mkpath( iconsDirPath ) ){
-            qDebug () << "Graph::graphSaveToGraphMLFormat() - created icons directory:" << iconsDirPath;
+            qDebug () << "Graph::graphSaveToGraphMLFormat() - created icons subdir"
+                      << iconsDirPath;
+        }
+        else {
+            qDebug () << "Graph::graphSaveToGraphMLFormat() - ERROR creating subdir!";
         }
     }
 

src/forms/dialognodeedit.cpp
@@ -40,7 +40,7 @@
 #include <QPushButton>
 #include <QColorDialog>
 #include <QPixmap>
-#include <QGraphicsEffect>
+#include <QGraphicsColorizeEffect>
 
 SOCNETV_USE_NAMESPACE
 
@@ -65,14 +65,14 @@ DialogNodeEdit::DialogNodeEdit(QWidget *parent,
 
     QStringList shapesList;
     QStringList iconList;
-    shapesList << "Box"
-                << "Circle"
-                << "Diamond"
-                << "Ellipse"
-                << "Triangle"
-                << "Star"
-                << "Bug"
-                << "Custom Icon";
+    shapesList << "box"
+                << "circle"
+                << "diamond"
+                << "ellipse"
+                << "triangle"
+                << "star"
+                << "bugs"
+                << "custom";
     iconList << ":/images/box.png"
              << ":/images/circle.png"
              << ":/images/diamond.png"
@@ -89,7 +89,7 @@ DialogNodeEdit::DialogNodeEdit(QWidget *parent,
     }
 
     ui->nodeIconSelectButton->setEnabled(false);
-    ui->nodeIconSelectEdit->setEnabled(false);
+    ui->nodeIconSelectEdit->setEnabled(false);/*
 
     if ( nodeShape == "box"  ){
        ui->nodeShapeComboBox->setCurrentIndex(0);
@@ -120,8 +120,39 @@ DialogNodeEdit::DialogNodeEdit(QWidget *parent,
         if (!iconPath.isEmpty()) {
             ui->nodeShapeComboBox->setItemIcon(7, QIcon(iconPath));
         }
-    }
+    }*/
 
+
+    int index = -1;
+    if ( (index = shapesList.indexOf(nodeShape)) != -1 ){
+
+        ui->nodeShapeComboBox->setCurrentIndex(index);
+
+        if ( index == NodeShape::Custom ) {
+
+            ui->nodeShapeComboBox->setCurrentIndex(NodeShape::Custom);
+            ui->nodeIconSelectButton->setEnabled(true);
+            ui->nodeIconSelectEdit->setEnabled(true);
+            ui->nodeIconSelectEdit->setText (iconPath);
+            if ( ! iconPath.isEmpty() ) {
+                ui->nodeShapeComboBox->setItemIcon(
+                            NodeShape::Custom,
+                            QIcon(iconPath));
+            }
+            else {
+                QGraphicsColorizeEffect *effect = new QGraphicsColorizeEffect;
+                effect->setColor(QColor("red"));
+                ui->nodeIconSelectButton->setGraphicsEffect(effect);
+                ui->nodeIconSelectEdit->setGraphicsEffect(effect);
+                (ui->buttonBox) -> button (QDialogButtonBox::Cancel) -> setDefault(true);
+                (ui->buttonBox) -> button (QDialogButtonBox::Ok) -> setEnabled(false);
+            }
+        }
+    }
+    else {
+        // default -- should never happen...
+        ui->nodeShapeComboBox->setCurrentIndex(NodeShape::Circle);
+    }
 
     pixmap = QPixmap(60,20) ;
     pixmap.fill(nodeColor);
@@ -194,6 +225,9 @@ void DialogNodeEdit::getNodeShape(const int &nodeShapeIndex){
         ui->nodeIconSelectEdit->setText(iconPath);
         if (!iconPath.isEmpty()) {
             ui->nodeShapeComboBox->setItemIcon(NodeShape::Custom, QIcon(iconPath));
+            ui->nodeIconSelectButton->setGraphicsEffect(0);
+            ui->nodeIconSelectEdit->setGraphicsEffect(0);
+
         }
         else {
             QGraphicsColorizeEffect *effect = new QGraphicsColorizeEffect;
@@ -229,6 +263,8 @@ void DialogNodeEdit::getNodeIconFile(){
     if (!m_nodeIconFile.isEmpty()) {
         qDebug() << m_nodeIconFile;
        ui->nodeIconSelectEdit->setText(m_nodeIconFile);
+       ui->nodeIconSelectButton->setGraphicsEffect(0);
+       ui->nodeIconSelectEdit->setGraphicsEffect(0);
        ui->nodeShapeComboBox->setItemIcon(NodeShape::Custom, QIcon(m_nodeIconFile));
        (ui->buttonBox) -> button (QDialogButtonBox::Ok) -> setEnabled(true);
     }

src/forms/dialogsettings.cpp
@@ -36,6 +36,7 @@
 #include <QMap>
 #include <QtDebug>
 
+#include <QGraphicsColorizeEffect>
 
 
 #include "global.h"
@@ -203,24 +204,37 @@ DialogSettings::DialogSettings(
        ui->nodeShapeComboBox->setItemIcon(i, QIcon(iconList[i]));
     }
 
-
     ui->nodeIconSelectButton->setEnabled(false);
     ui->nodeIconSelectEdit->setEnabled(false);
 
     int index = -1;
     if ( (index = shapesList.indexOf(m_appSettings["initNodeShape"])) != -1 ){
-        ui->nodeShapeComboBox->setCurrentIndex(index);
-        if ( index == NodeShape::Custom ) {
-            ui->nodeShapeComboBox->setCurrentIndex(NodeShape::Custom);
-            ui->nodeShapeComboBox->setItemIcon(NodeShape::Custom, QIcon(m_appSettings["initNodeIconPath"]));
 
+        ui->nodeShapeComboBox->setCurrentIndex(index);
+
+        if ( index == NodeShape::Custom ) {
+
+            ui->nodeShapeComboBox->setCurrentIndex(NodeShape::Custom);
             ui->nodeIconSelectButton->setEnabled(true);
             ui->nodeIconSelectEdit->setEnabled(true);
             ui->nodeIconSelectEdit->setText (m_appSettings["initNodeIconPath"]);
-
+            if ( ! m_appSettings["initNodeIconPath"].isEmpty() ) {
+                ui->nodeShapeComboBox->setItemIcon(
+                            NodeShape::Custom,
+                            QIcon(m_appSettings["initNodeIconPath"]));
+            }
+            else {
+                QGraphicsColorizeEffect *effect = new QGraphicsColorizeEffect;
+                effect->setColor(QColor("red"));
+                ui->nodeIconSelectButton->setGraphicsEffect(effect);
+                ui->nodeIconSelectEdit->setGraphicsEffect(effect);
+                (ui->buttonBox) -> button (QDialogButtonBox::Cancel) -> setDefault(true);
+                (ui->buttonBox) -> button (QDialogButtonBox::Ok) -> setEnabled(false);
+            }
         }
     }
     else {
+        // default -- should never happen...
         ui->nodeShapeComboBox->setCurrentIndex(NodeShape::Circle);
     }
 
@@ -593,14 +607,29 @@ void DialogSettings::getNodeShapeIndex(const int &shape){
          ui->nodeIconSelectButton->setEnabled(true);
          ui->nodeIconSelectEdit->setEnabled(true);
          ui->nodeIconSelectEdit->setText (m_appSettings["initNodeIconPath"]);
+
          if (!m_appSettings["initNodeIconPath"].isEmpty()) {
              emit setNodeShape(0, m_appSettings["initNodeShape"], m_appSettings["initNodeIconPath"]);
          }
+         else {
+             QGraphicsColorizeEffect *effect = new QGraphicsColorizeEffect;
+             effect->setColor(QColor("red"));
+             ui->nodeIconSelectButton->setGraphicsEffect(effect);
+             ui->nodeIconSelectEdit->setGraphicsEffect(effect);
+             (ui->buttonBox) -> button (QDialogButtonBox::Cancel) -> setDefault(true);
+             (ui->buttonBox) -> button (QDialogButtonBox::Ok) -> setEnabled(false);
+         }
+
      }
      else {
          ui->nodeIconSelectButton->setEnabled(false);
          ui->nodeIconSelectEdit->setEnabled(false);
          ui->nodeIconSelectEdit->setText ("");
+         ui->nodeIconSelectButton->setGraphicsEffect(0);
+         ui->nodeIconSelectEdit->setGraphicsEffect(0);
+         (ui->buttonBox) -> button (QDialogButtonBox::Ok) -> setDefault(true);
+         (ui->buttonBox) -> button (QDialogButtonBox::Ok) -> setEnabled(true);
+
          // emit signal
          emit setNodeShape(0, m_appSettings["initNodeShape"], QString() );
      }

src/mainwindow.cpp
@@ -8979,10 +8979,13 @@ void MainWindow::slotEditNodePropertiesDialog() {
 
     }
     else   {
+        qDebug() << "MW::slotEditNodePropertiesDialog() - "
+                    "selectedNodesCount" << selectedNodesCount;
+
         foreach (nodeNumber, activeGraph->graphSelectedVertices() ) {
             qDebug() << "MW::slotEditNodePropertiesDialog() "
-                        "changing properties for selected node "
-                     << nodeNumber ;
+                        "reading properties of selected node"
+                     << nodeNumber;
             if ( selectedNodesCount > 1 ) {
                 color = activeGraph->vertexColor( nodeNumber );
                 shape = activeGraph->vertexShape( nodeNumber);
@@ -9000,6 +9003,13 @@ void MainWindow::slotEditNodePropertiesDialog() {
     }
 
     //@todo add some grouping function here?
+
+    qDebug() << "MW::slotEditNodePropertiesDialog() - opening DialogNodeEdit."
+             << "label"<<label
+             << "size"<<size
+             << "color"<<color
+             << "shape"<<shape
+             << "iconPath"<<iconPath;
 
     m_nodeEditDialog = new DialogNodeEdit(this, label, size, color, shape, iconPath) ;
 

src/parser.cpp
@@ -2315,12 +2315,12 @@ void Parser::endGraphMLElementNode(QXmlStreamReader &xml){
     if (relationsList.count() > 1 ) {
         qDebug()<<"Parser::endGraphMLElementNode() - multirelational data"
                   "skipping node creation. Node should have been created in earlier relation";
-            bool_node = false;
+        bool_node = false;
         return;
     }
 
     qDebug()<<"Parser::endGraphMLElementNode() - signal to create node "
-           << " nodenumber "<< totalNodes  << " id " << node_id
+           << totalNodes  << " id " << node_id
            << " label " << nodeLabel << " coords " <<randX << ", " <<randY;
 
     if ( nodeShape == "custom") {
@@ -2443,30 +2443,41 @@ void Parser::endGraphMLElementEdge(QXmlStreamReader &xml){
 }
 
 
-/*
- * this method reads data for edges and nodes
+/**
+ * @brief Reads data for edges and nodes
  * called at a data element (usually nested inside a node or an edge element)
+ * @param xml
  */
 void Parser::readGraphMLElementData (QXmlStreamReader &xml){
+
     QXmlStreamAttributes xmlStreamAttr = xml.attributes();
     key_id = xmlStreamAttr.value("key").toString();
     key_value=xml.text().toString();
+
     qDebug()<< "Parser::readGraphMLElementData() - key_id: "
             <<  key_id <<  " key_value "<< key_value;
+
     if (key_value.trimmed() == "")
     {
-        qDebug()<< "Parser::readGraphMLElementData() - text: " << key_value;
+        qDebug()<< "Parser::readGraphMLElementData() - empty key_value: "
+                << key_value
+                << "reading more xml.text()...";
+
         xml.readNext();
+
         key_value=xml.text().toString();
-        qDebug()<< "Parser::readGraphMLElementData() - text: " << key_value;
-        if (  key_value.trimmed() != "" ) { //if there's simple text after the StartElement,
+
+        qDebug()<< "Parser::readGraphMLElementData() - now key_value: " << key_value;
+
+        if (  key_value.trimmed() != "" ) {
+            //if there's simple text after the StartElement,
             qDebug()<< "Parser::readGraphMLElementData() - key_id " << key_id
                     << " value is simple text " <<key_value ;
         }
         else {  //no text, probably more tags. Return...
             qDebug()<< "Parser::readGraphMLElementData() - key_id " << key_id
                     << " for " <<keyFor.value(key_id)
-                    << ". More elements nested here, continuing";
+                    << ". More elements nested here. Returning";
             return;
         }
 

commit 38b53f33640a7082fb4c8d3216e788053b2cf58c

Fixed #26 - Finally fixed regression mentioned in previous commit 547b396. We needed to set m_graphHasVertexCustomIcons=true when changing a subset of nodes to have custom icons

src/graph.cpp
@@ -702,47 +702,60 @@ void Graph::relationsClear(){
  * @param nodeShape
  * @param signalMW
  */
-void Graph::vertexCreate(const int &num,
-                         const int &nodeSize,
-                         const QString &nodeColor,
+void Graph::vertexCreate(const int &number,
+                         const int &size,
+                         const QString &color,
                          const QString &numColor,
                          const int &numSize,
                          const QString &label,
                          const QString &labelColor,
                          const int &labelSize,
                          const QPointF &p,
-                         const QString &nodeShape,
-                         const QString &nodeIconPath,
+                         const QString &shape,
+                         const QString &iconPath,
                          const bool &signalMW) {
 
     int value = 1;
 
-    qDebug() << "Graph::vertexCreate() - vertex:" << num
-             << "shape:" << nodeShape
-             << "icon:" << nodeIconPath
+    qDebug() << "Graph::vertexCreate() - vertex:" << number
+             << "shape:" << shape
+             << "icon:" << iconPath
              << "signalMW:" << signalMW
-             << "- Calling vertexAdd() and emitting signalDrawNode() to GW";
+             << "- Adding new vertex and emitting signalDrawNode() to GW";
 
-    vertexAdd ( num,
-                value,
-                nodeSize,
-                nodeColor,
-                numColor,
-                numSize,
-                label,
-                labelColor,
-                labelSize,
-                p,
-                nodeShape,
-                nodeIconPath);
+    if (order)
+        vpos[number]=m_totalVertices;
+    else
+        vpos[number]=m_graph.size();
+
+    m_graph.append(
+                new GraphVertex (
+                    this,
+                    number,
+                    value,
+                    m_curRelation ,
+                    size,
+                    color,
+                    numColor,
+                    numSize,
+                    label,
+                    labelColor,
+                    labelSize,
+                    p,
+                    shape,
+                    iconPath
+                    )
+                );
+
+    m_totalVertices++;
 
 
     emit signalDrawNode( p,
-                         num,
-                         nodeSize,
-                         nodeShape,
-                         nodeIconPath,
-                         nodeColor,
+                         number,
+                         size,
+                         shape,
+                         iconPath,
+                         color,
                          numColor,
                          numSize,
                          initVertexNumberDistance,
@@ -751,17 +764,18 @@ void Graph::vertexCreate(const int &num,
                          labelSize,
                          initVertexLabelDistance);
 
-    qDebug() << "Graph::vertexCreate() - vertex:" << num << "created. Calling graphSetModified().";
+    qDebug() << "Graph::vertexCreate() - Added new vertex:" << number
+             << "Calling graphSetModified().";
 
     graphSetModified(GraphChange::ChangedVertices, signalMW);
 
-    //to draw new user-clicked nodes with the same style of the file loaded:
+    //to draw new vertices by user with the same style of the file loaded:
     //save color, size and shape as init values
-    initVertexColor=nodeColor;
-    initVertexSize=nodeSize;
-    initVertexShape=nodeShape;
-    if (nodeShape=="custom"){
-        initVertexIconPath=nodeIconPath;
+    initVertexColor=color;
+    initVertexSize=size;
+    initVertexShape=shape;
+    if (shape=="custom"){
+        initVertexIconPath=iconPath;
     }
 } 
 
@@ -860,71 +874,6 @@ void Graph::vertexRemoveDummyNode(int i){
 
 }
 
-
-
-/**
- * @brief  Adds a vertex named v1, valued val, sized nszm colored nc, labeled nl,
- * labelColored lc, shaped nsp, at point p.
- * This method is called by vertexCreate() method
- * @param v1
- * @param val
- * @param size
- * @param color
- * @param numColor
- * @param numSize
- * @param label
- * @param labelColor
- * @param labelSize
- * @param p
- * @param shape
- */
-void Graph::vertexAdd ( const int &v1,
-                        const int &val,
-                        const int &size,
-                        const QString &color,
-                        const QString &numColor,
-                        const int &numSize,
-                        const QString &label,
-                        const QString &labelColor,
-                        const int &labelSize,
-                        const QPointF &p,
-                        const QString &shape,
-                        const QString &iconPath) {
-
-    qDebug() << "Graph::vertexAdd() ";
-
-    if (order)
-        vpos[v1]=m_totalVertices;
-    else
-        vpos[v1]=m_graph.size();
-
-    m_graph.append(
-                new GraphVertex (
-                    this,
-                    v1,
-                    val,
-                    m_curRelation ,
-                    size,
-                    color,
-                    numColor,
-                    numSize,
-                    label,
-                    labelColor,
-                    labelSize,
-                    p,
-                    shape,
-                    iconPath
-                    )
-                );
-
-    m_totalVertices++;
-
-//    qDebug() << "Graph: vertexAdd(): vertex" << m_graph.back()->name()
-//             << " appended with vpos= "<<vpos[v1]
-//             << " Now, m_graph size " << m_graph.size()
-//             << ". New vertex position: " << p.x() << "," << p.y();
-
-}
 
 
 
@@ -1462,6 +1411,7 @@ void Graph::vertexShapeSet(const int &v1, const QString &shape, const QString &i
                  << "new shape:" << shape
                  << "iconPath:" <<iconPath;
         m_graph[ vpos[v1] ]->setShape(shape, iconPath);
+        if (shape=="custom") { m_graphHasVertexCustomIcons = true; }
         emit setNodeShape(v1, shape, iconPath);
     }
     graphSetModified(GraphChange::ChangedVerticesMetadata);
@@ -14940,6 +14890,10 @@ bool Graph::graphSaveToGraphMLFormat (const QString &fileName,
         else {
             qDebug () << "Graph::graphSaveToGraphMLFormat() - ERROR creating subdir!";
         }
+    }
+    else {
+        qDebug () << "Graph::graphSaveToGraphMLFormat() - No custom node icons."
+                     << "Nothing to do";
     }
 
     QString iconPath = QString();

src/graph.h
@@ -137,17 +137,17 @@ public slots:
     void relationCurrentRename(const QString &newName, const bool &notifyMW=false);
 
     /** Slots to signals from Parser */
-    void vertexCreate ( const int &num,
+    void vertexCreate (const int &number,
                         const int &size,
-                        const QString &nodeColor,
+                        const QString &color,
                         const QString &numColor,
                         const int &numSize,
                         const QString &label,
-                        const QString &lColor,
+                        const QString &labelColor,
                         const int &labelSize,
                         const QPointF &p,
-                        const QString &nodeShape,
-                        const QString &nodeIconPath = QString::null,
+                        const QString &shape,
+                        const QString &iconPath = QString::null,
                         const bool &signalMW = false
             );
 
@@ -1124,19 +1124,6 @@ private:
     WebCrawler_Spider *wc_spider;
 
     /** private member functions */
-
-    void vertexAdd  (const int &v1,
-                     const int &val,
-                     const int &size,
-                     const QString &color,
-                     const QString &numColor,
-                     const int &numSize,
-                     const QString &label,
-                     const QString &labelColor,
-                     const int &labelSize,
-                     const QPointF &p,
-                     const QString &shape,
-                     const QString &iconPath=QString::null);
 
     void edgeAdd (const int &v1,
                   const int &v2,