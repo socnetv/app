1c632a82fc4731aa6bb37f952e9fb9da98e7b894
Dimitris Kalamaras <dimitris.kalamaras@gmail.com>
2024-11-28 23:23:51 +0200

    working on failing macos build in [travis]

--- scripts/travis_make_build_macos.sh
+++ scripts/travis_make_build_macos.sh
@@ -1,61 +1,50 @@
 #!/bin/bash
 
+
+set -e # Exit on any error
+set -o pipefail # Ensure errors in pipelines are caught
+
 echo "************************************************"
 echo "* STAGE 'script':  Building SocNetV for macOS  *"
 echo "************************************************"
 
-# Set the project name as app name
+# Constants
 APP_NAME="SocNetV"
-
-# Check current directory
 project_dir=$(pwd)
-echo "Project dir is: ${project_dir}"
+LAST_COMMIT_SHORT=$(git rev-parse --short HEAD)
+# linuxdeployqt always uses the output of 'git rev-parse --short HEAD' as the version.
+# We change this as follows:
+# If this is a tag, then version will be the tag, i.e. 2.5 or 2.5-beta
+# If this is not a tag, the we want version to be like "2.5-beta-a0be9cd"
+VERSION=${TRAVIS_TAG:-"${SOCNETV_VERSION}-${LAST_COMMIT_SHORT}"}
 
+# Print basic vars
+echo "PATH: $PATH"
 echo "TRAVIS_OS_NAME = $TRAVIS_OS_NAME"
 echo "TRAVIS_TAG = $TRAVIS_TAG"
 echo "TRAVIS_COMMIT = $TRAVIS_COMMIT"
 echo "SOCNETV_VERSION = $SOCNETV_VERSION"
 echo "TAG_NAME = ${TAG_NAME}"
 
-LAST_COMMIT_SHORT=$(git rev-parse --short HEAD)
-echo "LAST_COMMIT_SHORT = $LAST_COMMIT_SHORT"
 
-#
-# NOTE:
-#
-# linuxdeployqt always uses the output of 'git rev-parse --short HEAD' as the version.
-# We change this by exporting $VERSION environment variable
-#
+echo "Project dir: ${project_dir}"
+echo "Building version: ${VERSION}"
 
-echo "Checking TRAVIS_TAG to fix the VERSION..."
-if [ ! -z "$TRAVIS_TAG" ] ; then
-    # If this is a tag, then version will be the tag, i.e. 2.5 or 2.5-beta
-    VERSION=${TRAVIS_TAG}
-else 
-    # If this is not a tag, the we want version to be like "2.5-beta-a0be9cd"
-    VERSION=${SOCNETV_VERSION}-${LAST_COMMIT_SHORT}
-fi
-echo "VERSION = ${VERSION}";
-
-
-# SYSTEM CHECK
 # Ensure Qt is installed and in PATH
 export PATH="$(brew --prefix qt)/bin:$PATH"
 
-# Debugging: Check if qmake is found
 if ! command -v qmake &> /dev/null; then
-    echo "Error: qmake not found. Ensure Qt is installed and added to PATH."
+    echo "Error: qmake not found. Ensure Qt is installed via Homebrew."
     exit 1
 fi
 
-echo ""
-echo "qmake version = "
+echo "qmake version:"
 qmake -version
 
-# Print macOS version
-echo ""
-echo "macOS version = "
-sw_vers
+# Print system info
+echo "macOS version: $(sw_vers -productVersion)"
+echo "Xcode SDK Path: $(xcrun -sdk macosx --show-sdk-path)"
+
 
 # Print build env
 echo "Xcode build version = "
@@ -72,13 +61,20 @@ echo "Entering project dir:"
 cd ${project_dir}
 
 echo "Running qmake to configure it as release..."
-qmake QMAKE_APPLE_DEVICE_ARCHS="x86_64 arm64" -config release 
+qmake QMAKE_APPLE_DEVICE_ARCHS="x86_64 arm64" -config release || {
+    echo "Error: qmake failed."
+    exit 1
+}
 
 echo "Running make to compile the source code..."
-make -j$(sysctl -n hw.ncpu)
+make -j$(sysctl -n hw.ncpu) || {
+    echo "Error: make failed."
+    exit 1
+}
 
-echo "Finished building! Current dir contents now:"
-find .
+
+echo "Build complete. Verifying contents..."
+find . -type f -name "${APP_NAME}*"
 
 
 # Package the app
@@ -92,76 +88,89 @@ cd ${project_dir}/
 
 # Remove build directories that we don't want to deploy
 echo "Removing items we do not deploy from project dir ${project_dir}..."
-rm -rf moc
-rm -rf obj
-rm -rf qrc
+rm -rf moc obj qrc
 
-echo "Contents of ${APP_NAME}.app:"
-find ${APP_NAME}.app -type f
+# echo "Contents of ${APP_NAME}.app:"
+# find ${APP_NAME}.app -type f
 
 echo "Calling macdeployqt to create dmg archive from ${APP_NAME}.app:"
-macdeployqt ${APP_NAME}.app -dmg
+macdeployqt "${APP_NAME}.app" -dmg || {
+    echo "Error: macdeployqt failed."
+    exit 1
+}
 
-echo "Finished macdeployqt -- ${APP_NAME}.app now has these files inside:"
-find ${APP_NAME}.app -type f
+# Ensure universal binaries (x86_64 and arm64) are consistently included, using lipo:
+lipo -info ${APP_NAME}.app/Contents/MacOS/${APP_NAME}
 
-echo "Check if ${APP_NAME}.dmg has been created:"
-find . -type f -name ${APP_NAME}.dmg
 
-echo "Rename dmg archive to ${APP_NAME}-${VERSION}.dmg ..."
-mv ${APP_NAME}.dmg "${APP_NAME}-${VERSION}.dmg"
+# echo "Finished macdeployqt -- ${APP_NAME}.app now has these files inside:"
+# find ${APP_NAME}.app -type f
 
-echo "Check if there is a .dmg file created..."
-find . -type f -name *.dmg
+# Rename and package
+DMG_FILE="${APP_NAME}-${VERSION}.dmg"
+echo "Rename dmg archive to ${DMG_FILE} ..."
+if [[ -f ${APP_NAME}.dmg ]]; then
+    mv "${APP_NAME}.dmg" "${DMG_FILE}"
+else
+    echo "Error: DMG creation failed. No DMG file found."
+    exit 1
+fi
 
-# Copy other project files
-echo "Copying other project files..."
-cp "${project_dir}/README.md" "README.md"
-cp "${project_dir}/COPYING" "LICENSE"
+echo "Verifying DMG contents:"
+hdiutil attach ./${APP_NAME}-${VERSION}.dmg -nobrowse || exit 1
 
-echo "Creating zip archive..."
-7z a ${APP_NAME}-${VERSION}-macos.zip "${APP_NAME}-${VERSION}.dmg" "README.md" "LICENSE"
+# Create the zipped archive
+7z a "${APP_NAME}-${VERSION}-macos.zip" "${DMG_FILE}" README.md COPYING || {
+    echo "Error: Failed to create zip archive."
+    exit 1
+}
+
 
 # Remove the first DMG file
 [[ -f ${APP_NAME}-${VERSION}.dmg ]] && rm ${APP_NAME}-${VERSION}.dmg
 
-echo "Creating another dmg file with create-dmg..."
-echo "Creating release subdirectory..."
-mkdir -p release
+
+if ! command -v create-dmg &> /dev/null; then
+    echo "create-dmg not found. Skipping alternate DMG creation step."
+else
+    echo "Using create-dmg for additional DMG creation..."
+    echo "Creating release subdirectory..."
+    mkdir -p release
 
 
-echo "Moving app and extra files inside the release subdirectory..."
-mv ${APP_NAME}.app release/
-cp README.md release/
-cp LICENSE release/
+    echo "Moving app and extra files inside the release subdirectory..."
+    mv ${APP_NAME}.app release/
+    cp README.md release/
+    cp LICENSE release/
 
-#    --background $project_dir/packaging/macosx/DMG-Background.png \
-create-dmg \
-    --volname ${APP_NAME}-${VERSION} \
-    --volicon $project_dir/src/images/socnetv.icns \
-    --window-pos 200 120 \
-    --window-size 800 400 \
-    --icon-size 100  \
-    --icon "${APP_NAME}" 200 205 \
-    --hide-extension "${APP_NAME}.app" \
-    --app-drop-link 600 205 \
-    ./${APP_NAME}-${VERSION}.dmg \
-    ./release
+    # --background $project_dir/packaging/macosx/DMG-Background.png \
 
-#echo "Calling productbuild to create product archive .pkg from ${APP_NAME}.app:"
-#productbuild --component ${APP_NAME}.app /Applications "${APP_NAME}-${VERSION}.pkg"
+    create-dmg \
+        --volname ${APP_NAME}-${VERSION} \
+        --volicon $project_dir/src/images/socnetv.icns \
+        --window-pos 200 120 \
+        --window-size 800 400 \
+        --icon-size 100  \
+        --icon "${APP_NAME}" 200 205 \
+        --hide-extension "${APP_NAME}.app" \
+        --app-drop-link 600 205 \
 
-# You can use the appdmg command line app to create your dmg file if
-# you want to use a custom background and icon arrangement. I'm still
-# working on this for my apps, myself. If you want to do this, you'll
-# remove the -dmg option above.
-# appdmg json-path ${APP_NAME}_${TRAVIS_TAG}.dmg
+        ./${APP_NAME}-${VERSION}.dmg \
+        ./release
 
-echo "Check what we have created..."
+        echo "Verifying DMG contents:"
+        hdiutil attach ./${APP_NAME}-${VERSION}.dmg -nobrowse || exit 1
+
+fi
+
+
+
+
+echo "Release files:"
 find . -type f -name "${APP_NAME}*"
 
-echo " "
-echo "travis_build_macos: DONE. Returning to main script..."
-echo " "
+echo "************************************************"
+echo "* Build and packaging completed successfully.  *"
+echo "************************************************"
 
 exit 0
