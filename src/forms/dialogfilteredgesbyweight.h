83e1a1539addb3ce00baf4b0ee1d74c7018a0c5c
Dimitris Kalamaras <dimitris.kalamaras@gmail.com>
2025-02-25 00:27:47 +0200

    updated header comments

--- src/forms/dialogfilteredgesbyweight.h
+++ src/forms/dialogfilteredgesbyweight.h
@@ -1,29 +1,18 @@
-/***************************************************************************
- SocNetV: Social Network Visualizer
- version: 3.2
- Written in Qt
- 
-                         dialogfilteredgesbyweight.h  -  description
-                             -------------------
-    copyright         : (C) 2005-2023 by Dimitris B. Kalamaras
-    project site      : https://socnetv.org
+/**
+ * @file dialogfilteredgesbyweight.h
+ * @brief Defines the DialogFilterEdgesByWeight class for filtering edges based on weight in the social network visualizer.
+ * @author Dimitris B. Kalamaras
+ * @copyright
+ *   Copyright (C) 2005-2024 by Dimitris B. Kalamaras.
+ *   This file is part of SocNetV (Social Network Visualizer).
+ * @license
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, version 3 or later.
+ *   For more details, see <http://www.gnu.org/licenses/>.
+ * @see https://socnetv.org
+ */
 
- ***************************************************************************/
-
-/*******************************************************************************
-*     This program is free software: you can redistribute it and/or modify     *
-*     it under the terms of the GNU General Public License as published by     *
-*     the Free Software Foundation, either version 3 of the License, or        *
-*     (at your option) any later version.                                      *
-*                                                                              *
-*     This program is distributed in the hope that it will be useful,          *
-*     but WITHOUT ANY WARRANTY; without even the implied warranty of           *
-*     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            *
-*     GNU General Public License for more details.                             *
-*                                                                              *
-*     You should have received a copy of the GNU General Public License        *
-*     along with this program.  If not, see <http://www.gnu.org/licenses/>.    *
-********************************************************************************/
 
 #ifndef DIALOGFILTEREDGESBYWEIGHT_H
 #define DIALOGFILTEREDGESBYWEIGHT_H
