e1c89d1134ab6a124dfdc8f01fbbea4894745a96
Dimitris Kalamaras <dimitris.kalamaras@gmail.com>
2025-03-31 19:32:29 +0300

    start implementing a dialog to filter nodes according to their centrality scores -- maybe related to #108 ?

---
+++ src/forms/dialogfilternodesbycentrality.cpp
@@ -0,0 +1,44 @@
+/**
+ * @file dialogfilternodes.cpp
+ * @brief Implements the DialogFilterNodesByCentrality class for filtering nodes based on their attributes.
+ * @author Dimitris B. Kalamaras
+ * @copyright
+ *   Copyright (C) 2005-2025 by Dimitris B. Kalamaras.
+ *   This file is part of SocNetV (Social Network Visualizer).
+ * @license
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, version 3 or later.
+ *   For more details, see <http://www.gnu.org/licenses/>.
+ * @see https://socnetv.org
+ */
+
+#include "dialogfilternodesbycentrality.h"
+#include <QPushButton>
+#include <QDebug>
+
+DialogFilterNodesByCentrality::DialogFilterNodesByCentrality(QWidget *parent) : QDialog(parent)
+{
+	ui.setupUi(this);
+	connect(ui.buttonBox, SIGNAL(accepted()), this, SLOT(getUserChoices()));
+
+	(ui.buttonBox)->button(QDialogButtonBox::Ok)->setDefault(true);
+
+	(ui.overThresholdBt)->setChecked(true);
+}
+
+void DialogFilterNodesByCentrality::getUserChoices(){
+	qDebug()<< "Dialog: gathering Data!...";
+	bool overThreshold=false;
+	float my_threshold = static_cast <float> ( (ui.weightThreshold)->value() );
+	if ( ui.overThresholdBt->isChecked() ) {
+		qDebug()<< "Dialog: We will filter edges weighted more than threshold: " << my_threshold;
+		overThreshold = true;
+	}
+	else {
+		qDebug()<< "Dialog: We will filter edges weighted less than threshold: " << my_threshold;
+		overThreshold = false;
+	}	
+	qDebug()<< "Dialog: emitting userChoices" ;
+	emit userChoices( my_threshold, overThreshold );		
+}
---
+++ src/forms/dialogfilternodesbycentrality.h
@@ -0,0 +1,40 @@
+/**
+ * @file dialogfilterednodes.h
+ * @brief Defines the DialogFilterNodesByCentrality class for filtering nodes based on some attribute.
+ * @author Dimitris B. Kalamaras
+ * @copyright
+ *   Copyright (C) 2005-2025 by Dimitris B. Kalamaras.
+ *   This file is part of SocNetV (Social Network Visualizer).
+ * @license
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, version 3 or later.
+ *   For more details, see <http://www.gnu.org/licenses/>.
+ * @see https://socnetv.org
+ */
+
+#ifndef DIALOGFILTERNODES_H
+#define DIALOGFILTERNODES_H
+
+
+#include <QDialog>
+
+#include "ui_dialogfilternodesbycentrality.h"
+
+
+class DialogFilterNodesByCentrality : public QDialog
+{
+    Q_OBJECT
+public:
+    explicit DialogFilterNodesByCentrality (QWidget *parent = Q_NULLPTR);
+public slots:
+    void getUserChoices ();
+signals:
+    void userChoices(const qreal, const bool);
+private:
+    Ui::DialogFilterNodesByCentrality ui;
+
+};
+
+
+#endif
